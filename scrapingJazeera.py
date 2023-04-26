58d172c90cdfaac8fe7a594b859fc8db48041ddc
roma <boutouilarromaissae20@gmail.com>
2023-04-27 00:27:33 +0100

    with 1000

--- scrapingJazeera.py
+++ scrapingJazeera.py
@@ -3,23 +3,31 @@ from bs4 import BeautifulSoup
 import urllib.request
 import csv
 
-filecsv = open('PoliticsArticles.csv', 'w',encoding='utf-8-sig')
+filecsv = open('PoliticsArticlesExp4.csv', 'w',encoding='utf-8-sig')
 
 
 url = 'https://www.aljazeera.net/politics/'
 data = {}
-csv_columns = ['title','article']
+csv_columns = ['title','article','categorie']
+writer = csv.DictWriter(filecsv, fieldnames=csv_columns)
+
+writer.writeheader()
 r = requests.get(url)
+article_count = 0
+
 soup = BeautifulSoup(r.content, "html.parser")
 allarticles = soup.find_all("div", {'class': 'gc__content'})
-writer = csv.DictWriter(filecsv, fieldnames=csv_columns)
-i=0
-writer.writeheader()
-for pt in allarticles :
-    title = pt.find('h3', {'class' : 'gc__title'})
-    article = pt.find('div', {'class' : 'gc__body-wrap'})
-    writer.writerow({'title': title.text.strip(), 'article': article.text.strip()})
-    data['title'] = title.text.strip()
-    data['article'] = article.text.strip()
+while article_count<1000:
+   for pt in allarticles :
+       title = pt.find('h3', {'class' : 'gc__title'})
+       article = pt.find('div', {'class' : 'gc__body-wrap'})
+       categorie = soup.find('div', {'class' : 'section-header__title'})
+       article_count+=1
+       writer.writerow({'title': title.text.strip(), 'article': article.text.strip(), 'categorie': categorie.text.strip()})
+       data['title'] = title.text.strip()
+       data['article'] = article.text.strip()
+       data['categorie'] = categorie.text.strip()
+       if article_count == 1000:  # Sortir de la boucle lorsque 1000 articles sont extraits
+           break
 
 filecsv.close()
